d55eb0063f82a01f327b124fdfbbd25287bb33ab
ShahinFallah <shahinfallah2006@gmail.com>
2025-03-31 20:53:18 +0330

    feat: add Sonner for toast notifications

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,12 +1,18 @@
 "use client"
 
 import StoreProvider from "@/lib/redux/StoreProvider"
+import { Toaster } from "sonner"
 
 type MainLayoutProps = {
   children: React.ReactNode
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <StoreProvider>{children}</StoreProvider>
+  return (
+    <StoreProvider>
+      {children}
+      <Toaster richColors position="top-center" />
+    </StoreProvider>
+  )
 }
 export default MainLayout
