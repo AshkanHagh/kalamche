7822314204cf09ab1807e0f11c85875e4695a6de
ShahinFallah <shahinfallah2006@gmail.com>
2025-07-04 16:22:05 +0330

    feat(filters): add filtering logic to ProductRefineSearchInput component

--- client/src/app/(browse)/_components/input/ProductRefineSearchInput.tsx
+++ client/src/app/(browse)/_components/input/ProductRefineSearchInput.tsx
@@ -1,23 +1,53 @@
 "use client"
 
 import SearchBar from "@/components/search/SearchBar"
-import { cn } from "@/lib/utils"
+import { useRouter, useSearchParams } from "next/navigation"
+import { ChangeEvent, useEffect, useState } from "react"
 
 type ProductRefineSearchInputProps = {
   searchScopeName: string
-  className?: string
 }
 
 const ProductRefineSearchInput = ({
-  searchScopeName,
-  className
+  searchScopeName
 }: ProductRefineSearchInputProps) => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  const [inputValue, setInputValue] = useState("")
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+  }
+
+  useEffect(() => {
+    const currentSearchValue = searchParams.get("relatedSearch")
+    if (currentSearchValue) {
+      setInputValue(currentSearchValue)
+    }
+  }, [])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const params = new URLSearchParams(searchParams.toString())
+      if (inputValue) {
+        params.set("relatedSearch", inputValue)
+      } else {
+        params.delete("relatedSearch")
+      }
+      router.push(`?${params.toString()}`)
+    }, 1000)
+
+    return () => clearTimeout(timeout)
+  }, [inputValue])
+
   return (
     <SearchBar
       placeholder={`Search in ${searchScopeName}...`}
-      className={cn(className)}
-      onChange={(e) => console.log(e.target.value)}
+      onChange={handleChange}
+      value={inputValue}
     />
   )
 }
+
 export default ProductRefineSearchInput
