6029bff34a65c9568f0dc6996371193e6d6c72bc
ShahinFallah <shahinfallah2006@gmail.com>
2025-07-10 16:09:29 +0330

    style(skeleton): update layout of MainSkeleton and FilterPanelSkeleton for improved responsiveness

--- client/src/app/(browse)/_components/skeleton/main-skeleton.tsx
+++ client/src/app/(browse)/_components/skeleton/main-skeleton.tsx
@@ -9,9 +9,12 @@ export const MainSkeleton = () => {
         <Skeleton className="h-7 w-24 rounded-md" />
         <Skeleton className="h-5 w-80 rounded-md" />
       </div>
-      <div className="grid grid-cols-7 gap-x-5 mb-6">
-        <Skeleton className="h-8 col-span-6 rounded-md" />
-        <Skeleton className="h-8 rounded-md" />
+      <div className="flex flex-col md:flex-row justify-between gap-3 mb-6">
+        <Skeleton className="h-8 md:w-[80%] lg:w-[88%] rounded-md" />
+        <div className="flex gap-2 md:w-[20%] lg:w-[13%]">
+          <Skeleton className="h-8 rounded-md w-full" />
+          <Skeleton className="h-8 rounded-md w-full lg:hidden" />
+        </div>
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
@@ -30,7 +33,7 @@ export const MainSkeleton = () => {
 
 export const FilterPanelSkeleton = () => {
   return (
-    <div className="p-6 rounded-lg shadow shadow-primary/15 h-fit">
+    <div className="hidden p-6 rounded-lg shadow shadow-primary/15 h-fit lg:block">
       <div className="flex justify-between py-2">
         <Skeleton className="h-6 w-16 rounded-md" />
         <Skeleton className="h-6 w-16 rounded-md" />
